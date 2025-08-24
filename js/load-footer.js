c7faed5625722ec7ec4305bd13996c8049afb2ec
SeaPie <diguld@proton.me>
2025-08-24 20:55:24 +0200

    Removed footer.html and updated css

--- js/load-footer.js
+++
@@ -1,7 +0,0 @@
-function loadFooter() {
-    fetch('../HTML/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data;
-        });
-}
